3a66d712b30e4988ec6f950f887d7b023002feb2
quuger <greb.05@bk.ru>
2025-02-18 18:05:37 +0300

    Add initial version of server

--- app/src/main/java/ru/hse/routemood/ServerApp.java
+++ app/src/main/java/ru/hse/routemood/ServerApp.java
@@ -1,11 +1,21 @@
 package ru.hse.routemood;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class ServerApp {
-    public String getGreeting() {
-        return "Hello World!";
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApp.class, args);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new ru.hse.routemood.ServerApp().getGreeting());
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name,
+                        @RequestParam(value = "location", defaultValue = "Russia") String location) {
+        return String.format("Hello, %s from %s!", name, location);
     }
 }
