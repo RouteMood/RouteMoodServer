53914e902f26a9d2185f64fc78251017170b65fa
Vinogradov Ilya <ilyavin05@mail.ru>
2025-03-06 21:12:23 +0300

    Code to folder app

--- run-service.sh
+++ run-service.sh
@@ -1,5 +1,2 @@
 ./gradlew build -x test
-cp build/libs/AuthService-0.0.1-SNAPSHOT.jar .
-docker-compose down
-docker image rm auth-service:latest
-docker-compose up
+java -jar app/build/libs/app-0.0.1-SNAPSHOT.jar
