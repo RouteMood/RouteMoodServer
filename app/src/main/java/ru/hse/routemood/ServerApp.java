fe5866c0f4bd6149bfab349c59f29ad0f87e75f9
Vinogradov Ilya <ilyavin05@mail.ru>
2025-03-21 20:46:01 +0300

    Added files which ivan removed

--- app/src/main/java/ru/hse/routemood/ServerApp.java
+++ app/src/main/java/ru/hse/routemood/ServerApp.java
@@ -1,18 +1,23 @@
 package ru.hse.routemood;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import ru.hse.routemood.gpt.GptHandler;
+import ru.hse.routemood.gptMessage.GptRequest;
 
-@SpringBootApplication
 public class ServerApp {
 
     public static void main(String[] args) {
+
         if (args.length < 1) {
             throw new RuntimeException("No config file was given");
         }
+
         GptHandler.tokenFileName = args[0];
         GptHandler.init();
-        SpringApplication.run(ServerApp.class, args);
+        System.out.println(GptHandler.makeRequest(
+            GptRequest.builder()
+                .latitude(59.92951508111041)
+                .longitude(30.41197525476372)
+                .request("Хочу крутой маршрут")
+                .build()));
     }
 }
