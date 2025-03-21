adb8a2d59b4c2ca0d71cb10bb6df22a7ec2a0028
Vinogradov Ilya <ilyavin05@mail.ru>
2025-03-21 20:43:30 +0300

    Removed quuger files

--- app/src/main/java/ru/hse/routemood/gptRequest/GptRequestNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.hse.routemood.gptRequest;
-
-public class GptRequestNotFoundException extends RuntimeException {
-
-    GptRequestNotFoundException(Long id) {
-        super("Could not find massage " + id);
-    }
-}
