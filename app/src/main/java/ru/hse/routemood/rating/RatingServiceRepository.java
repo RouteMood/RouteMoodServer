b446724246a9df992185f97e29d931e5ee3aebd5
quuger <greb.05@bk.ru>
2025-05-05 16:24:25 +0300

    Add annotations

--- app/src/main/java/ru/hse/routemood/rating/RatingServiceRepository.java
+++ app/src/main/java/ru/hse/routemood/rating/RatingServiceRepository.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import ru.hse.routemood.rating.models.RatingItem;
 
 public interface RatingServiceRepository extends JpaRepository<RatingItem, UUID> {
 
-    Optional<RatingItem> findById(UUID id);
+    @NonNull
+    @Override
+    Optional<RatingItem> findById(@NonNull UUID id);
 
     List<RatingItem> findAllByAuthorUsername(String authorUsername);
 }
