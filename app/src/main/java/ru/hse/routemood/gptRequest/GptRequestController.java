2885fb4c637761cd8d0e88024ac971379ccfb40c
quuger <greb.05@bk.ru>
2025-05-07 10:02:15 +0300

    Fix the order of latitude and longitude

--- app/src/main/java/ru/hse/routemood/gptRequest/GptRequestController.java
+++ app/src/main/java/ru/hse/routemood/gptRequest/GptRequestController.java
@@ -1,41 +1,45 @@
 package ru.hse.routemood.gptRequest;
 
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.hse.routemood.gpt.GptHandler;
 import ru.hse.routemood.gpt.JsonWorker.Route;
 import ru.hse.routemood.gpt.JsonWorker.RouteItem;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @AllArgsConstructor
 public class GptRequestController {
+
     @PostMapping("/gpt-request")
     ResponseEntity<Route> newGptRequest(@RequestBody GptRequest request) {
         return ResponseEntity.ok(GptHandler.makeRequest(
-                GptRequest.builder().latitude(request.getLatitude())
-                        .longitude(request.getLongitude())
-                        .request(request.getRequest()).
-                        build()));
+            GptRequest.builder().latitude(request.getLatitude())
+                .longitude(request.getLongitude())
+                .request(request.getRequest()).
+                build()));
     }
 
     @GetMapping("/gpt-fictive-message")
     ResponseEntity<Route> fictiveMessage(
-            @RequestParam(name = "longitude") Double longitude,
-            @RequestParam(name = "latitude") Double latitude,
-            @RequestParam(name = "request") String request) {
+        @RequestParam(name = "latitude") Double latitude,
+        @RequestParam(name = "longitude") Double longitude,
+        @RequestParam(name = "request") String request) {
 
         Double[][] array = {{59.92951508111041, 30.41197525476372},
-                {59.930146, 30.409942},
-                {59.931028, 30.408584},
-                {59.932061, 30.407955},
-                {59.933068, 30.408038},
-                {59.93405, 30.408656},
-                {59.935008, 30.409826},
-                {59.93603, 30.412245}};
+            {59.930146, 30.409942},
+            {59.931028, 30.408584},
+            {59.932061, 30.407955},
+            {59.933068, 30.408038},
+            {59.93405, 30.408656},
+            {59.935008, 30.409826},
+            {59.93603, 30.412245}};
 
         List<RouteItem> result = new ArrayList<>();
         for (Double[] doubles : array) {
